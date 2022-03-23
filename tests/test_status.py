5dbc871be6497563ab7394bd1da8cef3ee143fe9
Ram <rmn24in@gmail.com>
2022-03-23 08:59:37 +0000

    🧪 tests for status endpoint

---
+++ tests/test_status.py
@@ -0,0 +1,45 @@
+import pytest
+from app import create_app
+from tests.helpers import expected_data_within_get_response
+from tests.helpers import expected_data_within_put_response
+
+
+@pytest.fixture(scope="session")
+def app():
+    app = create_app()
+    return app
+
+
+@pytest.mark.usefixtures("live_server")
+def test_get_status_response(flask_test_client):
+    """_summary_: Function send an application id as
+    a key with endpoint & returns expected output
+
+    Args:
+        flask_test_client
+    """
+
+    expected_data = {
+        "Application id": "uuidv4",
+        "Questions": {"Q1": "NOT STARTED"},
+    }
+
+    expected_data_within_get_response(
+        flask_test_client, "/fund/status/uuidv4", expected_data, debug=True
+    )
+
+
+@pytest.mark.usefixtures("live_server")
+def test_update_status_response(flask_test_client):
+    """_summary_: Function send an application id as
+    a key with endpoint & returns expected output
+
+    Args:
+        flask_test_client
+    """
+
+    expected_data = {"Q1": "COMPLETED"}
+
+    expected_data_within_put_response(
+        flask_test_client, "/fund/status/uuidv4/Q1", expected_data, debug=True
+    )

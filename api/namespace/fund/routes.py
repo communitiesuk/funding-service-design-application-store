5f3c5dd79b3f7e424c34e1b5d1fbc30b776b285d
Ram <rmn24in@gmail.com>
2022-03-23 09:02:57 +0000

    Status GET & PUT routes added

--- api/namespace/fund/routes.py
+++ api/namespace/fund/routes.py
@@ -4,13 +4,69 @@ from api.namespace.fund.fund_ns import fund_ns
 from flask_restx import reqparse
 from flask_restx import Resource
 
-"""
-GET/DELETE all funds
-"""
+APPLICATIONS.get_status("uuidv4")
+output = APPLICATIONS.update_question_status_to_COMPLETED("uuidv4", "Q1")
+print(f"New status: {output}")
+
+
+@fund_ns.route("/status/<application_id>", methods=["GET"])
+class ApplicationStatus(Resource):
+    """Summary: Function is a get method which
+    returns status of each question page from
+    application with name/title of the question page.
+
+    Args:
+        application: Takes an application_id
+
+    Returns:
+        Status of a question from each page
+    """
+
+    status_params_parser = reqparse.RequestParser()
+    status_params_parser.add_argument(
+        "status",
+        type=str,
+        help="application status is set to NOT_STARTED  by default",
+    )
+
+    def get(self, application_id):
+        application_status = APPLICATIONS.get_status(application_id)
+        for application_id in application_status:
+            for status in application_status.values():
+                return {"Application id": application_id, "Questions": status}
+
+
+@fund_ns.route("/status/<application_id>/<question_name>", methods=["PUT"])
+class UpdateApplicationStatus(Resource):
+
+    """_summary_:
+        Function is a put method which
+        updates the question status from
+        "NOT STARTED" to "COMPLETED"
+    Args:
+        application_id (str): Takes an application and runs through
+        get status function to validate the application id against
+        the database.
+        question_name (str): Takes an question name and runs through
+        get status function to check the retrive the question
+    Returns:
+         returns updated status -> "COMPLETED"
+    """
+
+    def put(self, application_id, question_name):
+        question_status = APPLICATIONS.update_question_status_to_COMPLETED(
+            application_id=application_id, question_name=question_name
+        )
+
+        return {question_name: question_status}
 
 
 @fund_ns.route("/all_funds")
 class Fund(Resource):
+    """
+    GET/DELETE all funds
+    """
+
     query_params_parser = reqparse.RequestParser()
     query_params_parser.add_argument(
         "delete_key", type=str, help="key to clear data store"
@@ -21,13 +77,12 @@ class Fund(Resource):
         return APPLICATIONS.get_funds()
 
 
-"""
-POST a new application
-"""
-
-
 @fund_ns.route("/new_application")
 class NewApplication(Resource):
+    """
+    POST a new application
+    """
+
     @fund_ns.doc("create_application")
     @fund_ns.expect(application_model_inbound)
     @fund_ns.marshal_with(application_model_inbound, code=201)
@@ -35,16 +90,15 @@ class NewApplication(Resource):
         return APPLICATIONS.create_application(fund_ns.payload), 201
 
 
-"""
-GET all applications for a fund 'fund/{fund-name}'
-OR GET all applications for a fund in a specified period
-'fund/{fund-name}/<date range parameters>'
-OR GET a specific application fund/fund-name?application_id={Id}
-"""
-
-
 @fund_ns.route("/<string:slugify_fund_name>")
 class Application(Resource):
+    """
+    GET all applications for a fund 'fund/{fund-name}'
+    OR GET all applications for a fund in a specified period
+    'fund/{fund-name}/<date range parameters>'
+    OR GET a specific application fund/fund-name?application_id={Id}
+    """
+
     query_params_parser = reqparse.RequestParser()
     query_params_parser.add_argument(
         "application_id", type=str, help="Application id"

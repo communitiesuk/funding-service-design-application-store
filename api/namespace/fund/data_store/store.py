c4423f9a983803c724a66af4cf1a10cd1768242e
Ram <rmn24in@gmail.com>
2022-03-23 09:03:37 +0000

    Get status & update status functions added

--- api/namespace/fund/data_store/store.py
+++ api/namespace/fund/data_store/store.py
@@ -1,3 +1,8 @@
+"""
+Data Access Object
+A data access object (DAO) is a pattern that provides an abstract interface
+to some type of database or other persistence mechanism.
+"""
 import datetime
 import json
 import uuid
@@ -8,24 +13,80 @@ from dateutil import parser as date_parser
 from dateutil.tz import UTC
 from slugify import slugify
 
-"""
-Data Access Object
-A data access object (DAO) is a pattern that provides an abstract interface
-to some type of database or other persistence mechanism.
-"""
-
 
 class ApplicationDataAccessObject(object):
+    """
+    return all funds (replace the date object with a string to send)
+    """
+
     def __init__(self):
         self.counter = 0
         self.funds = initial_fund_store_state
 
-    """
-        return all funds (replace the date object with a string to send)
-    """
+    def create_status(self, application):
+        """Summary:
+            Function create status & set to NOT_STARTED
+            by deafult to each question page
+        Args:
+            application: Takes an application/fund
+        """
+        for question in application["questions"]:
+            question["status"] = "NOT STARTED"
 
     def get_funds(self):
         return json.loads(json.dumps(self.funds, default=str))
+
+    def get_status(self, application_id):
+        """Summary:
+            Function returns status of each question page from
+            application with question page title/name.
+        Args:
+            application: Takes an application_id
+        Returns:
+            Status of a question from each page
+        """
+        status = {}
+        for funds in self.funds.values():
+            for fund in funds:
+                if application_id == fund["id"]:
+                    status[application_id] = {
+                        data.get("question"): data.get("status")
+                        for data in fund.get("questions")
+                    }
+        return status
+
+    def update_question_status_to_COMPLETED(
+        self, application_id, question_name
+    ):
+        """_summary_:
+            Function updates the question status from
+            "NOT STARTED" to "COMPLETED"
+        Args:
+            application_id (str): Takes an application and runs through
+            get status function to validate the application id against
+            the database.
+            question_name (str): Takes an question name and runs through
+            get status function to check the retrive the question data.
+        Returns:
+            _type_: returns updated status -> "COMPLETED"
+        """
+
+        status = ""
+        application_data = self.get_status(application_id)
+        if application_id in application_data:
+            for questions in application_data.values():
+                print(f"All questions: {questions}")
+                if question_name in questions:
+                    print(f"Question name: {question_name}")
+                    status += questions[question_name]
+                else:
+                    return f"Question name '{question_name}' does not exist"
+
+        else:
+            return f"Application id '{application_id}' does not exist"
+        print(f"Old status: {status}")
+        status = "COMPLETED"
+        return status
 
     def create_application(self, application):
         fund_name = slugify(application["name"])
@@ -36,8 +97,8 @@ class ApplicationDataAccessObject(object):
 
         if fund_name not in self.funds:
             self.funds[fund_name] = []
-
         self.funds[fund_name].append(application)
+        self.create_status(application)
         return application
 
     def get_applications_for_fund(
@@ -103,10 +164,10 @@ class ApplicationDataAccessObject(object):
             return "No key provided. Clear unsuccessful"
 
 
-"""
-An in memory data object instance
-"""
+# An in memory data object instance
+
 
 APPLICATIONS = ApplicationDataAccessObject()
+
 
 APPLICATIONS.create_application(initial_fund_store_application)

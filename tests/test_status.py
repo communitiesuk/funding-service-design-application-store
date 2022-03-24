78db12b39a801249e50279c3e458ba88a00e0502
Ram <rmn24in@gmail.com>
2022-03-24 10:28:31 +0000

    🧪 refactored

--- tests/test_status.py
+++ tests/test_status.py
@@ -2,24 +2,6 @@ from tests.helpers import expected_data_within_get_response
 from tests.helpers import expected_data_within_put_response
 
 
-def test_get_status_response(flask_test_client):
-    """_summary_: Function sends an application id as
-    a key with endpoint & returns expected output
-
-    Args:
-        flask_test_client
-    """
-
-    expected_data = {
-        "Application id": "uuidv4",
-        "Questions": {"Q1": "NOT STARTED"},
-    }
-
-    expected_data_within_get_response(
-        flask_test_client, "/fund/status/uuidv4", expected_data, debug=True
-    )
-
-
 def test_update_status_response(flask_test_client):
     """_summary_: Function sends an application_id
     as endpoint AND question & status as args
@@ -29,10 +11,8 @@ def test_update_status_response(flask_test_client):
         flask_test_client
     """
 
-    expected_data_NOT_STARTED = {
-        "Application id": "uuidv4",
-        "Questions": {"Q1": "NOT STARTED"},
-    }
+    expected_data_NOT_STARTED = {"Q1": "NOT STARTED"}
+
     expected_data_within_get_response(
         flask_test_client,
         "/fund/status/uuidv4",
@@ -40,10 +20,7 @@ def test_update_status_response(flask_test_client):
         debug=True,
     )
 
-    expected_data_IN_PROGRESS = {
-        "Application id": "uuidv4",
-        "Questions": {"Q1": "IN_PROGRESS"},
-    }
+    expected_data_IN_PROGRESS = {"Q1": "IN_PROGRESS"}
     expected_data_within_put_response(
         flask_test_client,
         "/fund/status/uuidv4" + "?new_status=IN_PROGRESS&question_name=Q1",
@@ -55,10 +32,8 @@ def test_update_status_response(flask_test_client):
         debug=True,
     )
 
-    expected_data_COMPLETED = {
-        "Application id": "uuidv4",
-        "Questions": {"Q1": "COMPLETED"},
-    }
+    expected_data_COMPLETED = {"Q1": "COMPLETED"}
+
     expected_data_within_put_response(
         flask_test_client,
         "/fund/status/uuidv4" + "?new_status=COMPLETED&question_name=Q1",

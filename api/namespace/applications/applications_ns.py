ed277720a54758c1cfc85d02c7204d50010ec8fe
harryyo <56394038+harryyo@users.noreply.github.com>
2022-04-09 19:50:40 +0100

    Namespace docstring reworded and moved to top

--- api/namespace/applications/applications_ns.py
+++ api/namespace/applications/applications_ns.py
@@ -1,9 +1,8 @@
+"""
+Namespace for bulk operations on applications
+"""
+
 from flask_restx import Namespace
-
-
-"""
-Namespace acts as part of an api (with the same methods as api)
-"""
 
 
 applications_ns = Namespace("applications", description="bulk application operations")

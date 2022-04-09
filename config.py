e555bec86991019950b03275b27c0d3834bc4927
harryyo <56394038+harryyo@users.noreply.github.com>
2022-04-09 19:43:34 +0100

    Docstrings replaced with comments

--- config.py
+++ config.py
@@ -2,17 +2,13 @@
 from os import environ
 from os import path
 
-"""
-Application Config
-"""
+#  Application Config
 SECRET_KEY = environ.get("SECRET_KEY") or "dev"
 SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME") or "session_cookie"
 FLASK_ROOT = path.dirname(path.realpath(__file__))
 FLASK_ENV = environ.get("FLASK_ENV") or "development"
 
-"""
-APIs Config
-"""
+#  APIs
 TEST_FUND_STORE_API_HOST = "fund_store"
 TEST_ROUND_STORE_API_HOST = "round_store"
 

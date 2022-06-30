8df6eb995f8431e873446ac3b471e5787f42adf1
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-06-30 15:56:31 +0100

    added noqa to top of file instead of each line

--- config/__init__.py
+++ config/__init__.py
@@ -1,18 +1,19 @@
+# flake8: noqa
 from os import environ
 
 FLASK_ENV = environ.get("FLASK_ENV")
 
 match FLASK_ENV:
     case "development":
-        from config.envs.development import DevelopmentConfig as Config  # noqa
+        from config.envs.development import DevelopmentConfig as Config
     case "dev":
-        from config.envs.dev import DevConfig as Config  # noqa
+        from config.envs.dev import DevConfig as Config
     case "test":
-        from config.envs.test import TestConfig as Config  # noqa
+        from config.envs.test import TestConfig as Config
     case "production":
-        from config.envs.production import ProductionConfig as Config  # noqa
+        from config.envs.production import ProductionConfig as Config
     case _:
-        from config.envs.default import DefaultConfig as Config  # noqa
+        from config.envs.default import DefaultConfig as Config
 
 if FLASK_ENV in ["development", "dev"]:
     Config.pretty_print()

29b603b1cc9b54cbe46bd2db653a2636071d92f7
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-08-15 14:31:43 +0100

    :pencil2: Reinstated README.

--- config/envs/dev.py
+++ config/envs/dev.py
@@ -11,9 +11,4 @@ from fsd_utils import configclass
 class DevConfig(DefaultConfig):
 
     FSD_LOGGING_LEVEL = logging.INFO
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + path.join(DefaultConfig.FLASK_ROOT, "sqlite.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

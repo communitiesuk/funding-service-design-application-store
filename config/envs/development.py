f5acca768090679d2af7b55afec0910b8583b6bf
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-08-17 20:42:16 +0100

    :bug: dev environment using sqlite

--- config/envs/development.py
+++ config/envs/development.py
@@ -12,10 +12,10 @@ class DevelopmentConfig(DefaultConfig):
 
     FSD_LOGGING_LEVEL = logging.DEBUG
 
-    SQLITE_DB_NAME = "sqlite.db"
     SQLALCHEMY_DATABASE_URI = environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///" + path.join(DefaultConfig.FLASK_ROOT, SQLITE_DB_NAME)
+    )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     FUND_ROUND_FORMS = {

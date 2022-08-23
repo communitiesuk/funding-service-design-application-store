d3d02600cfbb387522a1326d3b6e0a0855760eea
Adam <adam.wallace@communities.gov.uk>
2022-08-23 10:05:11 +0100

    allow dev db string override

--- config/envs/development.py
+++ config/envs/development.py
@@ -9,5 +9,6 @@ from fsd_utils import configclass
 @configclass
 class DevelopmentConfig(DefaultConfig):
 
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     FSD_LOGGING_LEVEL = logging.DEBUG
     SQLALCHEMY_TRACK_MODIFICATIONS = False

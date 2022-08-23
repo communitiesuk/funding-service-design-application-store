77d796ae7c1571ef1bc47ce70b5ab9538239d41f
Adam Wallace <adam.wallace@communities.gov.uk>
2022-08-23 12:10:41 +0100

    Fix fund config for test env (#26)
    Fund configuration on test incorrect. The test config was incorrectly overriding the base config.

--- config/envs/dev.py
+++ config/envs/dev.py
@@ -10,12 +10,3 @@ class DevConfig(DefaultConfig):
 
     FSD_LOGGING_LEVEL = logging.INFO
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    FUND_ROUND_FORMS = {
-        "fund-a:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-b:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-a:summer": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-b:summer": DefaultConfig.COF_R2_FORMS.copy(),
-        "funding-service-design:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "funding-service-design:summer": DefaultConfig.COF_R2_FORMS.copy(),
-    }
--- config/envs/test.py
+++ config/envs/test.py
@@ -10,16 +10,7 @@ class TestConfig(DefaultConfig):
 
     # Add any test specific config here
 
-    # Database
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
         "postgres://", "postgresql://"
     )
-
-    FUND_ROUND_FORMS = {
-        "fund-a:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-b:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-a:summer": DefaultConfig.COF_R2_FORMS.copy(),
-        "fund-b:summer": DefaultConfig.COF_R2_FORMS.copy(),
-        "funding-service-design:spring": DefaultConfig.COF_R2_FORMS.copy(),
-        "funding-service-design:summer": DefaultConfig.COF_R2_FORMS.copy(),
-    }
+    

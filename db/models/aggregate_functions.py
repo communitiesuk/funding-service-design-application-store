4d65d458740c966b9708996e3cb7f3e7f4c9f6d2
Gio Karunakaran <gio.karunakaran@version1.com>
2022-09-06 16:01:43 +0100

    :art: review changes

--- db/models/aggregate_functions.py
+++ db/models/aggregate_functions.py
@@ -154,10 +154,10 @@ def update_form(application_id, form_name, question_json):
     try:
         form_sql_row = FormsMethods.get_form(application_id, form_name)
         if form_sql_row.json[0]["fields"] != question_json[0]["fields"]:
+            ApplicationsMethods.application_edited(application_id)
             current_app.logger.info(
                 f"Application updated for application_id: '{application_id}."
             )
-            ApplicationsMethods.application_edited(application_id)
         form_sql_row.json = question_json
         update_statuses(application_id, form_name)
         db.session.commit()

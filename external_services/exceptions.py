90a7708002347d020cfb02d1ea2fcfff207e5ad2
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-09-26 15:19:29 +0100

    error message updated

--- external_services/exceptions.py
+++ external_services/exceptions.py
@@ -5,6 +5,6 @@ class NotificationError(Exception):
         message -- explanation of the error
     """
 
-    def __init__(self, message="Sorry, there was a problem to the notification service"):
+    def __init__(self, message="Sorry, there was a problem posting to the notification service"):
         self.message = message
         super().__init__(self.message)

a4f16cff8a3bba97ba5d01528358f6a463b0a744
Gio Karunakaran <gio.karunakaran@version1.com>
2022-10-07 16:01:02 +0100

    seperating script

--- db/models/aggregate_functions.py
+++ db/models/aggregate_functions.py
@@ -1,5 +1,3 @@
-
-
 import csv
 import datetime
 import io
@@ -14,8 +12,6 @@ from db.models.applications import ApplicationsMethods
 from db.models.forms import FormsMethods
 from flask import current_app
 from sqlalchemy.sql import func
-from api.routes.application.helpers import get_account
-from external_services.models.notification import Notification
 
 
 def update_application_status(application_id: str):
@@ -152,9 +148,8 @@ def get_round_name(fund_id, round_id):
         Config.FUND_STORE_API_HOST
         + Config.FUND_ROUND_ENDPOINT.format(fund_id=fund_id, round_id=round_id)
     )
-    # TO TEST OUT IN PROGRESS APPLICATIONS FUNCTIONALITY - DELETE IT 
-    send_email_on_deadline_task(fund_id, round_id)
-    
+    current_app.logger.error(f"fund_id: {fund_id}")
+    current_app.logger.error(f"round_id: {round_id}")
     if response:
         return response.get("title")
 
@@ -416,56 +411,3 @@ def get_report_for_all_applications():
                                 return_json[return_field] = field.get("answer")
         return_json_list.append(return_json)
     return return_json_list
-
-
-def send_email_on_deadline_task(fund_id, round_id):
-    
-    current_date_time = datetime.datetime.now().replace(microsecond=0)
-    current_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
-    fund_store_response = api.routes.application.helpers.get_data(
-        Config.FUND_STORE_API_HOST
-        + Config.FUND_ROUND_ENDPOINT.format(fund_id=fund_id, round_id=round_id)
-    )
-    fund_round_deadline = fund_store_response.get("deadline")
-    fund_round_name = fund_store_response.get("title")
-    
-    if current_date_time < fund_round_deadline: # Change < to >
-        current_app.logger.error("Current fund round has past the deadline")
-        
-        status = {"status_only": "IN_PROGRESS", "fund_id": fund_id, "round_id": round_id}
-        in_progress_applications = ApplicationsMethods.search_applications(
-            **status
-        )
-      
-        all_applications= []
-        for application in in_progress_applications:
-    
-                forms = FormsMethods.get_forms_by_app_id(application.get("id"))
-                application['forms'] = forms
-                application['round_name'] = fund_round_name
-                
-                # Update submission date temporarily to test out Notification response - DELETE after testing
-                application.update({"date_submitted":"2022-09-21T13:37:31.032064"})
-
-                all_applications.append({"application":application})
-                
-                 # application id & account to make a call to account store to get Applicant's email address
-                # retrieve email for each application from Magic link 
-                account_id = application.get("account_id")
-                account_store_response = get_account(account_id=account_id)
-                account_email = account_store_response.email
-            
-                # Post contents to Notification
-        if len(all_applications) > 0:
-            for application in all_applications:
-                Notification.send(template_type=Config.NOTIFY_TEMPLATE_SUBMIT_APPLICATION,to_email=account_email, content=application)
-                
-        else:
-            current_app.logger.error("There are no applications to be sent.")
-            
-            
-    else:
-        current_app.logger.error("Current fund round is active")
-  
-  
-    

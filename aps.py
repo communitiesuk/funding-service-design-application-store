732c5bdd78caf9710ee667e025de089246857307
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-10-10 12:40:56 +0100

    debugging db query

--- aps.py
+++ aps.py
@@ -8,9 +8,8 @@ from db.models.applications import ApplicationsMethods
 from db.models.forms import FormsMethods
 from external_services.models.notification import Notification
 from flask import current_app
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
 
 
 def send_email_on_deadline_task(fund_id, round_id):
@@ -18,12 +17,17 @@ def send_email_on_deadline_task(fund_id, round_id):
     current_date_time = (
         datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
     )
+    http_endpoint = "https://funding-service-design-fund-store-dev.london.cloudapps.digital/"
+    
     fund_rounds = api.routes.application.helpers.get_data(
-        Config.FUND_STORE_API_HOST
+        http_endpoint
         + Config.FUND_ROUND_ENDPOINT.format(fund_id=fund_id, round_id=round_id)
     )
 
-    if current_date_time > fund_rounds.get("deadline"):
+    current_app.logger.error( 
+       f" FUND STORE RESPONSE:::: {fund_rounds}"
+    )
+    if current_date_time < fund_rounds.get("deadline"):  # Change < to >
         status = {
             "status_only": "IN_PROGRESS",
             "fund_id": fund_id,
@@ -32,6 +36,8 @@ def send_email_on_deadline_task(fund_id, round_id):
         in_progress_applications = ApplicationsMethods.search_applications(
             **status
         )
+        
+        current_app.logger.error("I AM HERE")
 
         all_applications = []
         for application in in_progress_applications:
@@ -39,10 +45,18 @@ def send_email_on_deadline_task(fund_id, round_id):
                 application.get("id")
             )
             application["round_name"] = fund_rounds.get("title")
-            account_store_response = get_account(
+
+            # ----------------------------------------------------------------------
+            # Update submission date temporarily to test out Notification response - DELETE after testing # noqa
+            application.update(
+                {"date_submitted": "2022-09-21T13:37:31.032064"}
+            )
+            # ----------------------------------------------------------------------
+
+            account_id = get_account(
                 account_id=application.get("account_id")
             )
-            application["account_email"] = account_store_response.email
+            application["account_email"] = account_id.email
             all_applications.append({"application": application})
         count = 1
         if len(all_applications) > 0:
@@ -57,7 +71,7 @@ def send_email_on_deadline_task(fund_id, round_id):
                 )
                 # Add new template in Govuk-notify & in Notification service
                 Notification.send(
-                    template_type=Config.NOTIFY_TEMPLATE_SUBMIT_APPLICATION,
+                    template_type=Config.NOTIFY_TEMPLATE_SUBMIT_APPLICATION, 
                     to_email=email.get("email"),
                     content=application,
                 )

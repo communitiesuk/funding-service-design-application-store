824f13d56c373341d7d0bbe75320ff3dba775349
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-11-17 22:57:16 +0000

    wip add test

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,4 +1,6 @@
 from db.models.applications import ApplicationTestMethods
+from db.models.forms import Forms
+from db.models.forms import FormsMethods
 from tests.helpers import post_test_applications
 
 
@@ -23,4 +25,86 @@ def test_get_application_statuses(client):
     assert (
         response.data
         == b"NOT_STARTED,IN_PROGRESS,SUBMITTED,COMPLETED\r\n2,1,0,0\r\n"
+    )
+
+
+def test_get_applications_report(client):
+    post_test_applications(client)
+
+    application = ApplicationTestMethods.get_random_app()
+    application.status = "SUBMITTED"
+    section_put = {
+        "questions": [
+            {
+                "question": "About your organisation",
+                "fields": [
+                    {
+                        "name": "YdtlQZ",
+                        "title": "Applicant name",
+                        "type": "text",
+                        "answer": "Coolio",
+                    },
+                    {
+                        "key": "applicant-email",
+                        "title": "Email",
+                        "type": "text",
+                        "answer": "a@example.com",
+                    },
+                    {
+                        "key": "applicant-telephone-number",
+                        "title": "Telephone number",
+                        "type": "text",
+                        "answer": "Wow",
+                    },
+                    {
+                        "key": "applicant-website",
+                        "title": "Website",
+                        "type": "text",
+                        "answer": "www.example.com",
+                    },
+                ],
+            },
+            {
+                "question": "About your organisation",
+                "fields": [
+                    {
+                        "key": "data",
+                        "title": "Applicant name",
+                        "type": "text",
+                        "answer": "cool",
+                    },
+                ],
+            },
+            {
+                "question": "About your organisation",
+                "fields": [
+                    {
+                        "key": "data",
+                        "title": "Applicant job",
+                        "type": "text",
+                        "answer": "cool",
+                    },
+                ],
+            },
+        ],
+        "metadata": {
+            "application_id": application.id,
+            "form_name": "organisation-information",
+            "is_summary_page_submit": False,
+        },
+    }
+    client.put(
+        "/applications/forms",
+        json=section_put,
+        follow_redirects=True,
+    )
+
+    response = client.get(
+        "/applications/reporting/key_application_metrics",
+        follow_redirects=True,
+    )
+
+    assert (
+        response.data
+        == b"eoi_reference,organisation_name,asset_type,geography,organisation_type,capital,revenue\r\n"
     )

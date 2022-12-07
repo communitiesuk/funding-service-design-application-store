a39c85cbdb63b4314fa7b74ba9531db10a9c2421
Adam Wallace <adam.wallace@communities.gov.uk>
2022-12-07 13:54:53 +0000

    Submit application fix (#107)
    https://digital.dclg.gov.uk/jira/browse/FS-2032
    
    applications key no longer received from account store
    removed silent error and replaced with a throw 500 error on submit keyErrors
    added a test to check submit application returns expected body
    refactored/tidied tests

--- external_services/models/account.py
+++ external_services/models/account.py
@@ -1,17 +1,14 @@
 from dataclasses import dataclass
-from typing import List
 
 
 @dataclass
-class Account(object):
+class Account():
     id: str
     email: str
-    applications: List
 
     @staticmethod
     def from_json(data: dict):
         return Account(
             id=data["account_id"],
-            email=data["email_address"],
-            applications=data["applications"],
+            email=data["email_address"]
         )

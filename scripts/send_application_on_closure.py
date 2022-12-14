133ca4396d0fcde78f370264e29963a42c7c99e6
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-12-14 15:03:49 +0000

    Fixing imports, basic test to make sure it compiles

--- scripts/send_application_on_closure.py
+++ scripts/send_application_on_closure.py
@@ -5,13 +5,13 @@ from datetime import datetime
 
 sys.path.insert(1, ".")
 
-import external_services
-from app import app
-from config import Config
-from db.models.application.applications import ApplicationsMethods
-from db.models.forms import FormsMethods
-from external_services.models.notification import Notification
-from flask import current_app
+import external_services  # noqa: E402
+from app import app  # noqa: E402
+from config import Config  # noqa: E402
+from db.queries import search_applications  # noqa: E402
+from db.queries import get_forms_by_app_id  # noqa: E402
+from external_services.models.notification import Notification  # noqa: E402
+from flask import current_app  # noqa: E402
 
 
 def send_incomplete_applications_after_deadline(fund_id, round_id):
@@ -20,33 +20,27 @@ def send_incomplete_applications_after_deadline(fund_id, round_id):
         datetime.now().replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
     )
 
-    fund_rounds = external_services.get_data(
-        Config.FUND_STORE_API_HOST
-        + Config.FUND_ROUND_ENDPOINT.format(fund_id=fund_id, round_id=round_id)
-    )
+    fund_rounds = get_fund_round(fund_id, round_id)
     if current_date_time > fund_rounds.get("deadline"):
-        status = {
+        search_params = {
             "status_only": "IN_PROGRESS",
             "fund_id": fund_id,
             "round_id": round_id,
         }
-        in_progress_applications = ApplicationsMethods.search_applications(
-            **status
-        )
+        in_progress_applications = search_applications(**search_params)
         all_applications = []
         for application in in_progress_applications:
-            application["forms"] = FormsMethods.get_forms_by_app_id(
-                application.get("id")
-            )
+            application["forms"] = get_forms_by_app_id(application.get("id"))
             application["round_name"] = fund_rounds.get("title")
             account_id = external_services.get_account(
                 account_id=application.get("account_id")
             )
             application["account_email"] = account_id.email
             all_applications.append({"application": application})
-        count = 1
+        count = 0
         if len(all_applications) > 0:
             for application in all_applications:
+                count += 1
                 email = {
                     "email": application.get("account_email")
                     for application in application.values()
@@ -56,16 +50,25 @@ def send_incomplete_applications_after_deadline(fund_id, round_id):
                     f" {len(all_applications)} to {email.get('email')}"
                 )
                 Notification.send(
-                    template_type=Config.NOTIFY_TEMPLATE_INCOMPLETE_APPLICATION,
+                    template_type=Config.NOTIFY_TEMPLATE_INCOMPLETE_APPLICATION,  # noqa
                     to_email=email.get("email"),
                     content=application,
                 )
-                count += 1
-
+            current_app.logger.info(f"Sent {count} emails")
+            return count
         else:
             current_app.logger.info("There are no applications to be sent.")
+            return 0
     else:
         current_app.logger.info("Current round is active")
+        return -1
+
+
+def get_fund_round(fund_id, round_id):
+    return external_services.get_data(
+        Config.FUND_STORE_API_HOST
+        + Config.FUND_ROUND_ENDPOINT.format(fund_id=fund_id, round_id=round_id)
+    )
 
 
 def init_argparse() -> argparse.ArgumentParser:

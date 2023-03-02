1b5a022539c3b5be15c932dda96e7f20f5a4a77a
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-03-02 15:00:43 +0000

    WIP - trying nested transactions

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from flask import Response
 from flask_migrate import upgrade
 from tests.helpers import local_api_call
 
+# from sqlalchemy.orm import scoped_session
+# from sqlalchemy.orm import sessionmaker
+
 
 @pytest.fixture(scope="session")
 def app():
@@ -16,7 +19,36 @@ def app():
     app = create_app()
     with app.app_context():
         upgrade()
-    return app
+        conn = db.engine.connect()
+        nt = conn.begin_nested()
+
+        yield app
+
+        nt.rollback()
+        nt.close()
+
+    # with app.app_context():
+    #     upgrade()
+    #     # engines = db.engines
+
+    # # engine_cleanup = []
+
+    #     engine = db.engine
+    # # for key, engine in engines.items():
+    # # connection = engine.connect()
+    # # transaction = connection.begin()
+    # # session = db.create_scoped_session()
+    #     session = db.session
+    # # db.engine = connection
+    # # engine_cleanup.append((key, engine, connection, transaction))
+
+    #     yield app
+
+    # # for key, engine, connection, transaction in engine_cleanup:
+    # # transaction.rollback()
+    # # connection.close()
+    # # db.engine = engine
+    #     session.rollback()
 
 
 def mock_get_data(endpoint, params=None):
@@ -66,16 +98,16 @@ def mock_post_data_fix(mocker):
     )
 
 
-@pytest.fixture(scope="session")
-def _db(app):
-    """
-    Provide the transactional fixtures with access
-    to the database via a Flask-SQLAlchemy
-    database connection.
-    """
-    return db
+# @pytest.fixture(scope="session")
+# def _db(app):
+#     """
+#     Provide the transactional fixtures with access
+#     to the database via a Flask-SQLAlchemy
+#     database connection.
+#     """
+#     return db
 
 
-@pytest.fixture(autouse=True)
-def enable_transactional_tests(db_session):
-    pass
+# @pytest.fixture(autouse=True)
+# def enable_transactional_tests(db_session):
+#     pass

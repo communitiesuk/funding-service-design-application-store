a546c4f3622265ff2a260284d4a4a29a309fee48
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-03-15 13:49:40 +0000

    WIP test_routes working

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from app import create_app
-from db import db
+from db.queries.application import create_application
+from db.queries.form import add_new_forms
 from flask import Response
-from flask_migrate import upgrade
 from tests.helpers import local_api_call
 
 pytest_plugins = ["fsd_utils.fixtures.db_fixtures"]
@@ -16,19 +16,46 @@ def app():
     :return: A flask test client.
     """
     app = create_app()
-    with app.app_context():
-        upgrade()
     yield app
 
 
-@pytest.fixture(scope="session")
-def _db(app):
-    """
-    Provide the transactional fixtures with access
-    to the database via a Flask-SQLAlchemy
-    database connection.
-    """
-    return db
+@pytest.fixture(scope="function")
+def seed_application_records(
+    request, recreate_db, app, clear_test_data, enable_preserve_test_data
+):
+    marker = request.node.get_closest_marker("apps_to_insert")
+    if marker is None:
+        apps = [
+            {
+                "account_id": "usera",
+                "fund_id": "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4",
+                "round_id": "c603d114-5364-4474-a0c4-c41cbf4d3bbd",
+                "language": "en",
+            },
+        ]
+    else:
+        apps = marker.args[0]
+    seeded_ids = []
+    for app in apps:
+        app = create_application(**app)
+        add_new_forms(
+            [
+                "datganiadau"
+                if (app.language and app.language.name == "cy")
+                else "declarations"
+            ],
+            app.id,
+        )
+        add_new_forms(
+            [
+                "gwybodaeth-am-y-prosiect"
+                if (app.language and app.language.name == "cy")
+                else "project-information"
+            ],
+            app.id,
+        )
+        seeded_ids.append(app)
+    yield seeded_ids
 
 
 def mock_get_data(endpoint, params=None):

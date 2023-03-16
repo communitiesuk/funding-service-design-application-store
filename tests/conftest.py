ec099d4af9b896db387133ed12578d059bd11e61
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-03-16 15:11:03 +0000

    docs

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ from tests.helpers import local_api_call
 from tests.helpers import test_application_data
 from tests.helpers import test_question_data
 
+# Make the utils fixtures available, used in seed_application_records
 pytest_plugins = ["fsd_utils.fixtures.db_fixtures"]
 
 
@@ -28,6 +29,11 @@ def app():
 
 @pytest.fixture(scope="function")
 def unique_fund_round(mock_get_fund, mock_get_round):
+    """
+    Returns a tuple of 2 random uuids as fund_id and round_id.
+    Requests mock_get_fund and mock_get_round so when the app
+    looks up fund/round data it matches with these.
+    """
     return (str(uuid4()), str(uuid4()))
 
 
@@ -39,6 +45,15 @@ def seed_application_records(
     enable_preserve_test_data,
     unique_fund_round,
 ):
+    """
+    Inserts application data on a per-test (function scoped) basis
+    to prevent test pollution. Provides the inserted records to tests
+    so they can access them.
+
+    Each inserted application has 3 blank forms attached (declarations,
+    project-info, org-info, or their welsh equivalents depending
+    on language).
+    """
     marker = request.node.get_closest_marker("apps_to_insert")
     if marker is None:
         apps = [test_application_data[0]]
@@ -46,7 +61,7 @@ def seed_application_records(
         apps = marker.args[0]
     unique_fr_marker = request.node.get_closest_marker("unique_fund_round")
 
-    seeded_ids = []
+    seeded_apps = []
     for app in apps:
         if unique_fr_marker is not None:
             app["fund_id"] = unique_fund_round[0]
@@ -76,12 +91,19 @@ def seed_application_records(
             ],
             app.id,
         )
-        seeded_ids.append(app)
-    yield seeded_ids
+        seeded_apps.append(app)
+    yield seeded_apps
 
 
 @pytest.fixture(scope="function")
 def add_org_data_for_reports(seed_application_records, client):
+    """
+    Adds additional form data to the application so it is present for
+    testing the reports. Uses seed_application_records to update
+    all the applications that were inserted. Each org name is unique
+    in the format 'Test Org Name {n}' where n is the index+1 of the
+    application's position in seed_application_records.
+    """
     i = 0
     for application in seed_application_records:
         i += 1
@@ -148,6 +170,11 @@ def generate_mock_fund(fund_id: str) -> Fund:
 
 @pytest.fixture(scope="function", autouse=True)
 def mock_get_fund(mocker):
+    """
+    Generates a mock fund with the supplied fund ID.
+    Used with unique_fund_round to ensure when the fund and
+    round are retrieved, they match what's expected
+    """
     mocker.patch(
         "db.queries.application.queries.get_fund", new=generate_mock_fund
     )
@@ -168,6 +195,11 @@ def generate_mock_round(fund_id: str, round_id: str) -> Round:
 
 @pytest.fixture(scope="function", autouse=True)
 def mock_get_round(mocker):
+    """
+    Generates a mock round with the supplied fund and round IDs
+    Used with unique_fund_round to ensure when the fund and
+    round are retrieved, they match what's expected
+    """
     mocker.patch(
         "db.queries.application.queries.get_round", new=generate_mock_round
     )

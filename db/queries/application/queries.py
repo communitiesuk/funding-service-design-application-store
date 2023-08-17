b32adad483b807fc1d9cd5c67c906f2d55b151cd
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-08-17 17:54:03 +0100

    FS-3331 WIP json working for multi fund/round ids

--- db/queries/application/queries.py
+++ db/queries/application/queries.py
@@ -150,19 +150,50 @@ def get_all_applications() -> List:
 
 
 def get_count_by_status(
-    round_id: Optional[str] = None, fund_id: Optional[str] = None
+    round_ids: Optional[List] = None, fund_ids: Optional[List] = None
 ) -> Dict[str, int]:
-    query = db.session.query(Applications.status, func.count(Applications.status))
+    query = db.session.query(
+        Applications.fund_id,
+        Applications.round_id,
+        Applications.status,
+        func.count(Applications.status),
+    )
 
-    if round_id is not None:
-        query = query.filter_by(round_id=round_id)
-    if fund_id is not None:
-        query = query.filter_by(fund_id=fund_id)
+    if round_ids:
+        query = query.filter(Applications.round_id.in_(round_ids))
+    if fund_ids:
+        query = query.filter(Applications.fund_id.in_(fund_ids))
 
-    status_query = query.group_by(Applications.status).all()
-    statuses_with_counts = {status[0].name: status[1] for status in status_query}
-
-    return {**{s.name: 0 for s in ApplicationStatus}, **statuses_with_counts}
+    grouped_by_fund_round_result = (
+        query.group_by(Applications.fund_id)
+        .group_by(Applications.round_id)
+        .group_by(Applications.status)
+        .all()
+    )
+    results = []
+    unique_funds = set([f[0] for f in grouped_by_fund_round_result])
+    for fund_id in unique_funds:
+        unique_rounds = set(
+            row[1] for row in grouped_by_fund_round_result if row[0] == fund_id
+        )
+        rounds = []
+        for round_id in unique_rounds:
+            this_round_statuses = {
+                s[2].name: s[3]
+                for s in grouped_by_fund_round_result
+                if s[0] == fund_id and s[1] == round_id
+            }
+            rounds.append(
+                {
+                    "round_id": round_id,
+                    "metrics": {
+                        **{s.name: 0 for s in ApplicationStatus},
+                        **this_round_statuses,
+                    },
+                }
+            )
+        results.append({"fund_id": fund_id, "rounds": rounds})
+    return results
 
 
 def search_applications(**params):
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -39,8 +39,6 @@ user_lang_cy = {
     "language": "cy",
 }
 
-# @pytest.mark.apps_to_insert(test_application_data)
-
 
 @pytest.mark.fund_round_config(
     {
@@ -87,6 +85,94 @@ def test_get_application_statuses_json(client, seed_data_multiple_funds_rounds, 
     assert result["IN_PROGRESS"] == 2
     assert result["SUBMITTED"] == 0
     assert result["COMPLETED"] == 1
+
+
+@pytest.mark.fund_round_config(
+    {
+        "funds": [
+            {
+                "rounds": [
+                    {
+                        "applications": [
+                            {**user_lang_cy},
+                            {**user_lang_cy},
+                            {**user_lang},
+                        ]
+                    },
+                    {
+                        "applications": [
+                            {**user_lang},
+                        ]
+                    },
+                ]
+            },
+            {
+                "rounds": [
+                    {
+                        "applications": [
+                            {**user_lang_cy},
+                            {**user_lang_cy},
+                        ]
+                    }
+                ]
+            },
+        ]
+    }
+)
+@pytest.mark.parametrize(
+    "fund_idx, round_idx, exp_not_started, exp_in_progress, exp_submitted,"
+    " exp_completed",
+    [
+        ([0, 1], [], 0, 5, 0, 1),
+        ([0], [], 0, 3, 0, 1),
+        ([1], [], 0, 2, 0, 0),
+        ([], [0], 0, 2, 0, 1),
+    ],
+)
+def test_get_application_statuses_json_multi_fund(
+    fund_idx,
+    round_idx,
+    exp_not_started,
+    exp_in_progress,
+    exp_submitted,
+    exp_completed,
+    client,
+    seed_data_multiple_funds_rounds,
+    _db,
+):
+    app = get_row_by_pk(Applications, seed_data_multiple_funds_rounds[0][1][0][1][0])
+    app.status = "COMPLETED"
+    _db.session.add(app)
+    _db.session.commit()
+    fund_ids = [seed_data_multiple_funds_rounds[idx][0] for idx in fund_idx]
+    fund_params = ["fund_id=" + str(id) for id in fund_ids]
+    round_ids = [seed_data_multiple_funds_rounds[0][1][idx] for idx in round_idx]
+    round_params = ["round_id=" + str(id) for id in round_ids]
+    url = (
+        "/applications/reporting/applications_statuses_data?"
+        + f"format=json&{'&'.join(fund_params)}&{'&'.join(round_params)}"
+    )
+    response = client.get(url, follow_redirects=True)
+    result = response.json
+    assert result
+    funds = result["metrics"]
+    for fund_id in fund_ids:
+        assert (
+            len([fund["fund_id"] for fund in funds if fund["fund_id"] == fund_id]) == 1
+        )
+        total_ip = 0
+        total_ns = 0
+        total_c = 0
+        total_s = 0
+        for f in funds:
+            total_ns += sum([r["metrics"]["NOT_STARTED"] for r in f["rounds"]])
+            total_ip += sum([r["metrics"]["IN_PROGRESS"] for r in f["rounds"]])
+            total_c += sum([r["metrics"]["COMPLETED"] for r in f["rounds"]])
+            total_s += sum([r["metrics"]["SUBMITTED"] for r in f["rounds"]])
+        assert total_ns == exp_not_started
+        assert total_ip == exp_in_progress
+        assert total_c == exp_completed
+        assert total_s == exp_submitted
 
 
 @pytest.mark.fund_round_config(

dc41801cabb9588ec69caba7827bbb9e9212496d
srh-sloan <srh-sloan@users.noreply.github.com>
2024-02-21 11:19:48 +0000

    fs-4143 updating report mappings iwth new form names (#319)

--- config/key_report_mappings/cof_eoi_key_report_mapping.py
+++ config/key_report_mappings/cof_eoi_key_report_mapping.py
@@ -6,28 +6,28 @@ COF_EOI_KEY_REPORT_MAPPING = KeyReportMapping(
     round_id="6a47c649-7bac-4583-baed-9c4e7a35c8b3",
     mapping=[
         FormMappingItem(
-            form_name="cof-eoi-details",
+            form_name="organisation-details",
             key="SMRWjl",
             return_field="organisation_name",
         ),
         FormMappingItem(
-            form_name="cof-eoi-support",
+            form_name="development-support-provider",
             key="xWnVof",
             return_field="lead_contact_name",
         ),
         FormMappingItem(
-            form_name="cof-eoi-asset",
+            form_name="about-your-asset",
             key="Ihjjyi",
             return_field="asset_type",
         ),
         FormMappingItem(
-            form_name="cof-eoi-asset",
+            form_name="about-your-asset",
             key="dnqIdW",
             return_field="geography",
             formatter=extract_postcode,
         ),
         FormMappingItem(
-            form_name="cof-eoi-funding",
+            form_name="your-funding-request",
             key="fZAMFv",
             return_field="capital",
         ),

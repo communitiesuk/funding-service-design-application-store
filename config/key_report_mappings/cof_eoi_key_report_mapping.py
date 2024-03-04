b3f44c17a1fb77687b349565ec447348e7bafb7e
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-03-04 14:22:25 +0000

    fs-4243 added welsh form names

--- config/key_report_mappings/cof_eoi_key_report_mapping.py
+++ config/key_report_mappings/cof_eoi_key_report_mapping.py
@@ -7,32 +7,38 @@ COF_EOI_KEY_REPORT_MAPPING = KeyReportMapping(
     mapping=[
         FormMappingItem(
             form_name="organisation-details",
+            form_name_cy="manylion-y-sefydliad",
             key="SMRWjl",
             return_field="organisation_name",
         ),
         FormMappingItem(
             form_name="development-support-provider",
+            form_name_cy="darparwr-cymorth-datblygu",
             key="xWnVof",
             return_field="lead_contact_name",
         ),
         FormMappingItem(
             form_name="about-your-asset",
+            form_name_cy="ynglyn-ach-ased",
             key="Ihjjyi",
             return_field="asset_type",
         ),
         FormMappingItem(
             form_name="about-your-asset",
+            form_name_cy="ynglyn-ach-ased",
             key="dnqIdW",
             return_field="geography",
             formatter=extract_postcode,
         ),
         FormMappingItem(
             form_name="your-funding-request",
+            form_name_cy="eich-cais-am-gyllid",
             key="fZAMFv",
             return_field="capital",
         ),
         FormMappingItem(
             form_name="development-support-provider",
+            form_name_cy="darparwr-cymorth-datblygu",
             key="NQoGIm",
             return_field="applicant_email",
         ),

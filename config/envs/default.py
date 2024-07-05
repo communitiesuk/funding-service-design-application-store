be627d6bbdf9b6f662d1bd7ea1457ab0c2c5f7e2
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2024-07-05 16:53:08 +0100

    remove VCAP specific config

--- config/envs/default.py
+++ config/envs/default.py
@@ -1,5 +1,4 @@
 """Flask configuration."""
-import json
 import logging
 import os
 from os import environ
@@ -44,22 +43,6 @@ class DefaultConfig:
         AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
         AWS_SQS_IMPORT_APP_PRIMARY_QUEUE_URL = os.environ.get("PRIMARY_QUEUE_URL")
         AWS_SQS_IMPORT_APP_SECONDARY_QUEUE_URL = os.environ.get("DEAD_LETTER_QUEUE_URL")
-    elif "VCAP_SERVICES" in os.environ:
-        vcap_services = json.loads(os.environ["VCAP_SERVICES"])
-
-        if "aws-s3-bucket" in vcap_services:
-            s3_credentials = vcap_services["aws-s3-bucket"][0]["credentials"]
-            AWS_REGION = s3_credentials["aws_region"]
-            AWS_ACCESS_KEY_ID = s3_credentials["aws_access_key_id"]
-            AWS_SECRET_ACCESS_KEY = s3_credentials["aws_secret_access_key"]
-            AWS_BUCKET_NAME = s3_credentials["bucket_name"]
-        if "aws-sqs-queue" in vcap_services:
-            sqs_credentials = vcap_services["aws-sqs-queue"][0]["credentials"]
-            AWS_SQS_REGION = sqs_credentials["aws_region"]
-            AWS_SQS_ACCESS_KEY_ID = sqs_credentials["aws_access_key_id"]
-            AWS_SQS_SECRET_ACCESS_KEY = sqs_credentials["aws_secret_access_key"]
-            AWS_SQS_IMPORT_APP_PRIMARY_QUEUE_URL = sqs_credentials["primary_queue_url"]
-            AWS_SQS_IMPORT_APP_SECONDARY_QUEUE_URL = sqs_credentials["secondary_queue_url"]
     else:
         AWS_ACCESS_KEY_ID = AWS_SQS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
         AWS_SECRET_ACCESS_KEY = AWS_SQS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
